8b232d0cc389b17fc1f3d8ad855324256f08d014
Programador Cego <programadorcego@gmail.com>
2025-03-28 10:37:49 -0300

    Estrutura inicial

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const toggleMenu = document.querySelector('.toggle-menu');
+
+toggleMenu.addEventListener('click', e => {
+    const btn = e.target;
+    btn.classList.toggle('toggled');
+    let toggled = btn.classList.contains('toggled');
+    let maxHeight = 0;
+    let visibility = 'hidden';
+    btn.ariaExpanded = toggled;
+
+    if(toggled) {
+        maxHeight = document.querySelector('.menu-wrap').scrollHeight;
+        visibility = 'visible';
+    }
+
+    document.querySelector('.menu-wrap').style.visibility = visibility;
+    document.querySelector('.menu-wrap').style.maxHeight = maxHeight;
+});
