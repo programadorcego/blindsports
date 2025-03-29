2f4e2c0cc378bf4a41689ade6798d48237f44194
Programador Cego <programadorcego@gmail.com>
2025-03-29 11:47:10 -0300

    Botões para tamanho de fonte e contraste

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,51 @@ toggleMenu.addEventListener('click', e => {
     document.querySelector('.menu-wrap').style.visibility = visibility;
     document.querySelector('.menu-wrap').style.maxHeight = `${maxHeight}px`;
 });
+
+const root = document.querySelector(':root');
+let fontRootSize = localStorage.getItem('fontRootSize') || 16;
+fontRootSize = parseInt(fontRootSize);
+root.style.fontSize = `${fontRootSize}px`;
+
+let highContrast = !!localStorage.getItem('high-contrast');
+
+if(highContrast) document.body.classList.add('high-contrast');
+
+document.querySelector('.increase-font-size').addEventListener('click', () => {
+    if(fontRootSize < 32) {
+        fontRootSize += 2;
+        localStorage.setItem('fontRootSize', fontRootSize);
+    }
+
+    root.style.fontSize = `${fontRootSize}px`;
+});
+
+document.querySelector('.decrease-font-size').addEventListener('click', () => {
+    if(fontRootSize > 16) {
+        fontRootSize -= 2;
+        localStorage.setItem('fontRootSize', fontRootSize);
+    }
+
+    root.style.fontSize = `${fontRootSize}px`;
+});
+
+document.querySelector('.reset').addEventListener('click', () => {
+    fontRootSize = 16;
+    highContrast = false;
+
+    localStorage.removeItem('high-contrast');
+    localStorage.removeItem('fontRootSize');
+
+    document.body.classList.remove('high-contrast');
+
+    root.style.fontSize = `${fontRootSize}px`;
+});
+
+document.querySelector('.set-high-contrast').addEventListener('click', () => {
+    highContrast = !highContrast;
+    localStorage.setItem('high-contrast', highContrast)
+    
+    if(!highContrast) localStorage.removeItem('high-contrast');
+
+    document.body.classList.toggle('high-contrast');
+});
